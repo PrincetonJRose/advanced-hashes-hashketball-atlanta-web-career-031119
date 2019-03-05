54f6b2548369574885e7055f13314af0fbe2d8c1
Princeton Rose <decoyzpr@gmail.com>
2019-03-05 23:40:42 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -154,7 +154,7 @@ def team_colors(team_name)
   team_info = game_hash
   team_info.each do |travel, info|
     info.each do |more_info, more|
-      if more_info == :colors and more_info[:team_name] == team_name
+      if more_info == :colors
         return more
       end
     end
